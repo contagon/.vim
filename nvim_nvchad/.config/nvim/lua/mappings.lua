ed205d928b0c887cf457ccde8e9362364585c476
Easton Potokar <contagon6@gmail.com>
2024-10-23 17:15:06 -0400

    Nvchad -> kickstart.nvim

--- nvim_nvchad/.config/nvim/lua/mappings.lua
+++ nvim_nvchad/.config/nvim/lua/mappings.lua
@@ -9,9 +9,15 @@ local nomap = vim.keymap.del
 -- map("i", "jk", "<ESC>")
 
 -- Remap file search
--- nomap("n", "<leader>ff")
-map("n", "<C-p>", "<cmd>Telescope find_files<cr>", {desc = "VSCode file search"})
+nomap("n", "<leader>ff")
+map("n", "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", {desc = "VSCode file search"})
+
+-- Remap grep all files
+nomap("n", "<leader>x")
+map("n", "<C-w>", function()
+  require("nvchad.tabufline").close_buffer()
+end, { desc = "buffer close" })
 
 -- Remap file explorer
-nomap("n", "<C-n>")
+-- nomap("n", "<C-n>")
 map("n", "<C-b>", "<cmd>NvimTreeToggle<CR>", {desc = "VSCode toggle window"})
